86dd644872be0c56eff72d6b4890276d28b6f1b6
Dragon_X <barron.samuel111@gmail.com>
2025-02-03 21:03:49 -0800

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
 
-const nggUrl = 'https://schoprox.com/';
+const nggUrl = 'https://www.thejungla.com/';
 
 const proxy = createProxyMiddleware({
   target: nggUrl,
