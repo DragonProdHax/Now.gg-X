6a577f6ccc0745e6e3e5a367eb45395985a12c62
Dragon_X <barron.samuel111@gmail.com>
2025-02-03 19:07:17 -0800

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
 
-const nggUrl = 'https://github.com/';
+const nggUrl = 'https://play.prodigygame.com/';
 
 const proxy = createProxyMiddleware({
   target: nggUrl,
