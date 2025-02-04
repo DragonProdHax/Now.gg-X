80861b9e5c626ade73aee10944510f085dd19439
Dragon_X <barron.samuel111@gmail.com>
2025-02-03 20:58:44 -0800

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
 
-const nggUrl = 'https://www.youtubeunblocked.live/';
+const nggUrl = 'https://schoprox.com/';
 
 const proxy = createProxyMiddleware({
   target: nggUrl,
