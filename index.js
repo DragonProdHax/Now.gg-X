4125e9f1cc201175c3500ce68889baa40701b059
Dragon_X <barron.samuel111@gmail.com>
2025-02-03 19:01:31 -0800

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
 
-const nggUrl = 'https://doctoraux.com/';
+const nggUrl = 'https://classroom6x.gitlab.io/1v1-lol/';
 
 const proxy = createProxyMiddleware({
   target: nggUrl,
