62fca91de26a5e6e2fc81bfc35d6065cf41b1fc5
Dragon_X <barron.samuel111@gmail.com>
2025-02-06 16:47:35 -0800

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import { createProxyMiddleware } from 'http-proxy-middleware';
 
 const app = express();
-const nggUrl = 'https://algebra.learnnexus.one/';
+const nggUrl = 'https://embed.su/';
 
 const proxy = createProxyMiddleware({
   target: nggUrl,
